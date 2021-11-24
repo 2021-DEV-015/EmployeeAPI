179130d72f6afea090ce99b19025a3364130c417
sundara7 <sundar.n2006@gmail.com>
2021-11-24 19:07:41 +0530

    feat: Added employee service should return employee object when create service is called

---
+++ src/main/java/com/bnppf/employee/api/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.bnppf.employee.api.service;
+
+import com.bnppf.employee.api.domain.EmployeeDTO;
+
+public class EmployeeService {
+
+	public EmployeeDTO create() {
+		return new EmployeeDTO();
+	}
+
+}
---
+++ src/test/java/com/bnppf/employee/api/service/EmployeeServiceTest.java
@@ -0,0 +1,20 @@
+package com.bnppf.employee.api.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.bnppf.employee.api.domain.EmployeeDTO;
+
+@SpringBootTest
+public class EmployeeServiceTest {
+
+	@Test
+	public void shouldReturnCreatedEmployeeWhenCreateServiceIsCalled()
+			throws Exception {
+		EmployeeService service = new EmployeeService();
+		EmployeeDTO employee = service.create();
+
+		Assertions.assertNotNull(employee);
+	}
+}
