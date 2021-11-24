0d2d3f7952db90ea64b1b41fb389b223fd55167e
sundara7 <sundar.n2006@gmail.com>
2021-11-24 20:00:05 +0530

    test: refactor extracted integer and string literals as constant for better code reusability

--- src/test/java/com/bnppf/employee/api/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/bnppf/employee/api/repository/EmployeeRepositoryTest.java
@@ -16,16 +16,17 @@ import com.bnppf.employee.api.domain.Employee;
 @DataJpaTest
 public class EmployeeRepositoryTest {
 
+	private static final String DEPARTMENT_NAME = "department1";
 	private static final String ADDRESS = "22 Fairylane Circle, Dearborn, Michigan";
 	private static final String EMPLOYEE_NAME = "Employee1";
-	private static final int EMPLOYEE_ID = 1;
+	private static final int ID = 1;
 	@Autowired
 	private EmployeeRepository repository;
 
 	@Test
 	public void shouldSaveEmployeeWhenPassingEmployeeData() throws Exception {
 		Employee employee = new Employee();
-		employee.setId(EMPLOYEE_ID);
+		employee.setId(ID);
 		employee.setName(EMPLOYEE_NAME);
 		Calendar calendar = Calendar.getInstance();
 		employee.setDateOfBirth(new Date(calendar.getTimeInMillis()));
@@ -33,22 +34,22 @@ public class EmployeeRepositoryTest {
 
 		List<Department> departments = new ArrayList<Department>();
 		Department department = new Department();
-		department.setId(EMPLOYEE_ID);
-		department.setName("department1");
+		department.setId(ID);
+		department.setName(DEPARTMENT_NAME);
 		departments.add(department);
 		employee.setDepartments(departments);
 
 		Employee savedEmployee = repository.save(employee);
 
-		Assertions.assertEquals(1, savedEmployee.getId());
+		Assertions.assertEquals(ID, savedEmployee.getId());
 		Assertions.assertEquals(EMPLOYEE_NAME, savedEmployee.getName());
 		Assertions.assertEquals(ADDRESS, savedEmployee.getAddress());
 		Assertions.assertNotNull(savedEmployee.getDateOfBirth());
 		Assertions.assertEquals(employee.getDepartments().size(), savedEmployee
 				.getDepartments().size());
-		Assertions.assertEquals(EMPLOYEE_ID, savedEmployee.getDepartments()
-				.stream().findFirst().get().getId());
-		Assertions.assertEquals("department1", savedEmployee.getDepartments()
+		Assertions.assertEquals(ID, savedEmployee.getDepartments().stream()
+				.findFirst().get().getId());
+		Assertions.assertEquals(DEPARTMENT_NAME, savedEmployee.getDepartments()
 				.stream().findFirst().get().getName());
 	}
 }
