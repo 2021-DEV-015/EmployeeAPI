3053251d24c32e07233d9c643843fa81715ed2c7
sundara7 <sundar.n2006@gmail.com>
2021-11-25 00:22:33 +0530

    refactor: removing lombok data as departments entity fetch is not supporting using lombok

--- src/main/java/com/bnppf/employee/api/domain/Employee.java
+++ src/main/java/com/bnppf/employee/api/domain/Employee.java
@@ -6,15 +6,13 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.Data;
-
 @Entity
 @Table(name = "EMPLOYEE")
-@Data
 public class Employee {
 
 	@Id
@@ -29,7 +27,47 @@ public class Employee {
 	@Column(name = "ADDRESS")
 	private String address;
 
-	@OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<Department> departments;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public List<Department> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(List<Department> departments) {
+		this.departments = departments;
+	}
 
 }
